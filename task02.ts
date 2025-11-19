f9f78a9182be7d7000aafee887710c9c148bf69d
THJuang2025 <th.juang.2025@gmail.com>
2025-11-19 15:22:27 +0700

    feat:TODO-01: Print event or odd of the number

--- task02.ts
+++ task02.ts
@@ -1,5 +1,11 @@
 // TODO-02: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
 
-const userInput = "42";
+const userInput = NaN;
 // Try changing to "hello", 10, null, NaN
+if (typeof Number(userInput) === "number" && !isNaN(Number(userInput)) ) {
+    console.log("it's a number");
+}
+else
+    {console.log("***You NOT input number na ka!***");
+}
